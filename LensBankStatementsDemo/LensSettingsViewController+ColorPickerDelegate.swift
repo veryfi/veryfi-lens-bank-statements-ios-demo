86b74757773a3bca6a8d53b8a3f49736292f7159
Douglas Poveda <douglaspoveda@veryfi.com>
2023-10-22 21:25:25 -0500

    Add demo project

---
+++ LensBankStatementsDemo/LensSettingsViewController+ColorPickerDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  LensSettingsViewController+ColorPickerDelegate.swift
+//  LensBankStatementsDemo
+//
+//  Created by Bernardo Restrepo on 12/03/22.
+//
+
+import UIKit
+
+extension LensSettingsViewController: UIColorPickerViewControllerDelegate {
+    func colorPickerViewControllerDidFinish(_ viewController: UIColorPickerViewController) {
+        if let changingColor = changingColor, let indexPath = changingColorIndexPath {
+            if changingColor.1 == .stringColorCell {
+                jsonSettings[changingColor.0.rawValue] = viewController.selectedColor.toHexString()
+            }
+            settingsTableView.reloadRows(at: [indexPath], with: .automatic)
+            self.changingColor = nil
+            changingColorIndexPath = nil
+        }
+    }
+}
---
+++ LensBankStatementsDemo/LensSettingsViewController+TableViewDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  LensSettingsViewController+TableViewDelegate.swift
+//  LensBankStatementsDemo
+//
+//  Created by Bernardo Restrepo on 12/03/22.
+//
+
+import UIKit
+
+extension LensSettingsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let type = sections[indexPath.section][indexPath.row].1
+        if type == .stringValueCell || type == .doubleValueCell || type == .integerValueCell {
+            handleValueCellTap(indexPath: indexPath)
+        } else if type == .stringColorCell {
+            handleColorCellTap(indexPath: indexPath)
+        }
+    }
+}
